35ccbe46ae536fd4f26ade67c042dd5968a37e4e
elitenomad <stalin.pranava@gmail.com>
2016-03-26 19:08:48 +1100

    Add User specs and apis

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -16,4 +16,21 @@ describe Api::V1::UsersController do
 
     it { should respond_with 200 }
   end
+
+  describe "Post #create" do
+
+    context "when successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it 'renders the json response just created for the user ' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eq @user_attributes[:email]
+      end
+
+      it { is_expected.to respond_with 201}
+    end
+  end
 end
