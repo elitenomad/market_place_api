04956cac26af145ae9d5ae4c4f23fc0858f353ad
elitenomad <stalin.pranava@gmail.com>
2016-03-28 10:07:43 +1100

    Add sign In action api to the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, presence: true, uniqueness: true
+  validates :auth_token, uniqueness: true
+  before_save :generate_authentication_token!
+
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
