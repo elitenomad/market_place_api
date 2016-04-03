bcb6c5c42c5ad0ada622853dea7cf67e8b602c2e
elitenomad <stalin.pranava@gmail.com>
2016-04-03 15:42:58 +1000

    Add user serializzer for customizing the input

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -8,7 +8,7 @@ describe Api::V1::UsersController do
     end
 
     it "returns the information about a reporter on a hash" do
-      user_response = json_response
+      user_response = json_response[:user]
       expect(user_response[:email]).to eql @user.email
     end
 
@@ -24,7 +24,7 @@ describe Api::V1::UsersController do
       end
 
       it 'renders the json response just created for the user ' do
-        user_response = json_response
+        user_response = json_response[:user]
         expect(user_response[:email]).to eq @user_attributes[:email]
       end
 
@@ -42,7 +42,7 @@ describe Api::V1::UsersController do
       end
 
       it "renders the json representation for the updated user" do
-        user_response = json_response
+        user_response = json_response[:user]
         expect(user_response[:email]).to eql "xyz@simple.com"
       end
 
